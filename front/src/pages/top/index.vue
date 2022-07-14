10ac5b666821067fe72b4a949cc9e2bc9ac2c2b1
itoshusu <sopfantasy@yahoo.co.jp>
2022-07-15 06:28:00 +0900

    add: piniaの導入

--- front/src/pages/top/index.vue
+++ front/src/pages/top/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div>トップページです</div>
+  <div>{{ store.hello }}</div>
 </template>
 
 <script setup lang="ts">
-
+import { useStore } from '../../store/store';
+const store = useStore()
 </script>
 
 <style scoped>
---
+++ front/src/store/store.ts
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+
+interface State {
+  hello: string
+}
+
+export const useStore = defineStore('app', {
+  state: (): State => ({
+    hello: 'hello world',
+  }),
+  actions: {
+  }
+})
