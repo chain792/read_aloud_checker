adfbf1006a683d761389ddc2a3fa38a14c680595
itoshusu <sopfantasy@yahoo.co.jp>
2022-07-15 08:28:14 +0900

    add: vuetify実験

--- front/src/pages/top/index.vue
+++ front/src/pages/top/index.vue
@@ -4,14 +4,28 @@
   <v-btn
     flat
     color="secondary"
+    :size="a"
+    class="ma-lg-5"
   >
     Secondary
   </v-btn>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import { useStore } from '../../store/store';
+import { useDisplay } from 'vuetify'
 const store = useStore()
+const display = useDisplay()
+console.log(display.md) // 1280
+const a = computed(() => {
+  console.log('con')
+  if(display.md.value){
+    return 'large'
+  }else{
+    return 'small'
+  }
+})
 </script>
 
 <style scoped>
