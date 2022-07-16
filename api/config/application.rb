3b4fcf5b3cb2e93d920fbd8ff3df6892704be381
itoshusu <sopfantasy@yahoo.co.jp>
2022-07-17 06:54:10 +0900

    add: 自動読み込みパスの設定を追加

--- api/config/application.rb
+++ api/config/application.rb
@@ -37,5 +37,8 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # https://railsguides.jp/autoloading_and_reloading_constants.html#%24load-path-load-path
+    config.add_autoload_paths_to_load_path = false
   end
 end
