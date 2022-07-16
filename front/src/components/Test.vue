62fa83cab13faec5a8f9197b6a42fdf3360a48b9
itoshusu <sopfantasy@yahoo.co.jp>
2022-07-16 11:12:34 +0900

    test

---
+++ front/src/components/Test.vue
@@ -0,0 +1,10 @@
+<template>
+  <button><slot name="unko" :mmm="data">submit</slot></button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+const data = ref("hello")
+</script>
+
+<style scoped></style>
--- front/src/pages/test/index.vue
+++ front/src/pages/test/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div>TEST</div>
+<Test>
+  <template v-slot:unko="aiu">
+  aaaaaaaaas{{aiu.mmm}}
+  </template>
+</Test>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Test from "../../components/Test.vue"
+</script>
 
 <style scoped></style>
