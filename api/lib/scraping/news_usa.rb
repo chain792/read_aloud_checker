5cf3afce417a4f8c4db6c3fba2039052c47f833a
itoshusu <sopfantasy@yahoo.co.jp>
2022-07-25 16:44:20 +0900

    add: スクレイピング処理を追加

---
+++ api/lib/scraping/news_usa.rb
@@ -0,0 +1,18 @@
+module Scraping
+  class NewsUSA
+    def self.scrape
+      url = 'https://www.copyrightfreecontent.com/'
+      html = URI.open(url).read
+      doc = Nokogiri::HTML.parse(html)
+      news = []
+      doc.css('#content > div').each do |div|
+        break if div.attribute('class').value == 'navigation'
+        title = div.at_css('h1 > a').text
+        body = div.at_css('.entry > p').text.gsub('(NewsUSA) – ','')
+        category = div.at_css('h1 > a').attribute('href').value.gsub(url, '').gsub(/\/.*/, '')
+        news << { title: title, body: body, category: category }
+      end
+      news
+    end
+  end
+end
