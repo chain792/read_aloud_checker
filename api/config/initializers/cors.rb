e2cd83c53bfc57d379960b6d5d36a97784ba892f
itoshusu <sopfantasy@yahoo.co.jp>
2022-08-10 05:45:15 +0900

    debug

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000','https://ondoku-star.com'
+    origins ENV.fetch("FRONT_DOMAIN", '')
 
     resource '/api/v1/*',
       headers: :any,
