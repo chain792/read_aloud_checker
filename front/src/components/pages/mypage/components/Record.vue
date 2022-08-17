1fc2e264abfa2e84dad30a3771049cf38daa8369
itoshusu <sopfantasy@yahoo.co.jp>
2022-08-17 17:00:19 +0900

    add: 合計音読数と発音精度の表示を追加

--- front/src/components/pages/mypage/components/Record.vue
+++ front/src/components/pages/mypage/components/Record.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="select-container mt-5">
-    <v-select
-      v-model="itemValue"
-      :items="items"
-      variant="solo"
-      density="comfortable"
-    ></v-select>
+  <div class="mt-5 d-flex align-stretch">
+    <div class="select-title px-3">
+      <p class="mt-2">期間</p>
+    </div>
+    <div class="select-container">
+      <v-select
+        v-model="itemValue"
+        :items="items"
+        variant="solo"
+        density="compact"
+      ></v-select>
+    </div>
+  </div>
+  <div class="mt-6">
+    <p class="ml-16">合計音読数： {{ totalSucceededNumber }}単語</p>
+    <p class="ml-16">発音精度： {{ precision }}<span v-if="precision !== 'ー'">%</span></p>
   </div>
   <GChart
     type="ColumnChart"
@@ -15,7 +24,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, watch } from "vue"
+import { ref, reactive, watch, Ref } from "vue"
 import { GChart } from 'vue-google-charts'
 import axios from "@/plugins/axios"
 import { useUserStore } from "@/store/userStore"
@@ -30,6 +39,8 @@ interface Item {
   value: string
 }
 
+const totalSucceededNumber = ref(0)
+const precision: Ref<string | number> = ref(100)
 const date = new Date()
 const currentYear = date.getFullYear()
 const currentMonth = date.getMonth() + 1
@@ -61,7 +72,7 @@ watch(itemValue, () => {
 
 
 const chartData: any[][] = reactive([
-  ['日', '音読した単語数', 'ミスした単語数'],
+  ['日', '音読した単語', 'ミスした単語'],
 ])
 
 const fetchRecord = async (year: number | string, month: number | string): Promise<void> => {
@@ -72,10 +83,16 @@ const fetchRecord = async (year: number | string, month: number | string): Promi
         month
       }
     })
+    totalSucceededNumber.value = 0
+    let totalFailedNumber = 0
     chartData.splice(1)
     res.data.forEach((v: MonthlyRecord, i: number) => {
       chartData.push([`${month}/${i+1}`, v.succeededNumber, v.failedNumber])
+      totalSucceededNumber.value += v.succeededNumber
+      totalFailedNumber += v.failedNumber
     })
+    precision.value = (totalSucceededNumber.value + totalFailedNumber) === 0 ? 
+      'ー' : Math.round(totalSucceededNumber.value * 100 / (totalSucceededNumber.value + totalFailedNumber))
   } catch(e) {
     console.log(e)
   }
@@ -90,7 +107,17 @@ const chartOptions = {
 </script>
 
 <style scoped>
+.select-title{
+  background-color: rgba(220,220,220,.4);
+  border: 1px solid rgba(150,150,150,.4);
+}
 .select-container{
   width: 160px;
+  height: 40px;
+}
+</style>
+<style>
+.select-container .v-field__field{
+  padding-top: 2px !important;
 }
 </style>
