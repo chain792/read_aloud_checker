09a5ee065cff427853dba2399feac5305dcbe252
itoshusu <sopfantasy@yahoo.co.jp>
2022-08-17 18:44:10 +0900

    add: タブの選択情報をセッションストレージに保持する処理を追加

---
+++ front/src/components/pages/mypage/components/History.vue
@@ -0,0 +1,14 @@
+<template>
+あああ
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, watch, Ref } from "vue"
+import axios from "@/plugins/axios"
+import { useUserStore } from "@/store/userStore"
+
+
+</script>
+
+<style scoped>
+</style>
