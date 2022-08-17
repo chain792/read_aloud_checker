3651534df1aedb6b1151996d3cfe8253804174f0
itoshusu <sopfantasy@yahoo.co.jp>
2022-08-17 16:10:50 +0900

    add: プルダウンで選択した月のデータを取得する処理を追加

--- front/src/components/pages/mypage/components/Record.vue
+++ front/src/components/pages/mypage/components/Record.vue
@@ -1,5 +1,12 @@
 <template>
-  8月
+  <div class="select-container mt-5">
+    <v-select
+      v-model="itemValue"
+      :items="items"
+      variant="solo"
+      density="comfortable"
+    ></v-select>
+  </div>
   <GChart
     type="ColumnChart"
     :data="chartData"
@@ -8,20 +15,56 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from "vue"
+import { ref, reactive, watch } from "vue"
 import { GChart } from 'vue-google-charts'
 import axios from "@/plugins/axios"
+import { useUserStore } from "@/store/userStore"
+
 
 interface MonthlyRecord {
   succeededNumber: number
   failedNumber: number
 }
+interface Item {
+  title: string
+  value: string
+}
+
+const date = new Date()
+const currentYear = date.getFullYear()
+const currentMonth = date.getMonth() + 1
+const [registerdYear, registerdMonth] = useUserStore().authUser!.createdAt.split('-')
+const diffYear = currentYear - Number(registerdYear)
+const diffMonth = currentMonth - Number(registerdMonth)
+const itemNumber = diffYear * 12 + diffMonth + 1
+const items: Array<Item> = reactive([])
+let dy = 0
+let dm = 0
+for(let i = 0; i < itemNumber; i++) {
+  items.push({
+    title: `${currentYear - dy}年${currentMonth - dm}月`,
+    value: `${currentYear - dy}-${currentMonth - dm}`
+  })
+  if(currentMonth - dm < 1){
+    dy += 1
+    dm -= 12
+  }else{
+    dm += 1
+  }
+}
+const itemValue = ref(`${currentYear}-${currentMonth}`)
+
+watch(itemValue, () => {
+  const [year, month] = itemValue.value.split('-')
+  fetchRecord(year, month)
+})
+
 
 const chartData: any[][] = reactive([
   ['日', '音読した単語数', 'ミスした単語数'],
 ])
 
-const fetchRecord = async (year: number, month: number): Promise<void> => {
+const fetchRecord = async (year: number | string, month: number | string): Promise<void> => {
   try{
     const res = await axios.get(`user/trainings`, {
       params: {
@@ -29,6 +72,7 @@ const fetchRecord = async (year: number, month: number): Promise<void> => {
         month
       }
     })
+    chartData.splice(1)
     res.data.forEach((v: MonthlyRecord, i: number) => {
       chartData.push([`${month}/${i+1}`, v.succeededNumber, v.failedNumber])
     })
@@ -36,7 +80,7 @@ const fetchRecord = async (year: number, month: number): Promise<void> => {
     console.log(e)
   }
 }
-fetchRecord(2022, 8)
+fetchRecord(currentYear, currentMonth)
 
 
 
@@ -46,4 +90,7 @@ const chartOptions = {
 </script>
 
 <style scoped>
+.select-container{
+  width: 160px;
+}
 </style>
