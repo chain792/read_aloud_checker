542385aa1110974cc1b8d15c0709684aba3bb7e0
itoshusu <sopfantasy@yahoo.co.jp>
2022-08-23 05:01:04 +0900

    実験

--- api/config/deploy.rb
+++ api/config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
-set :application, "app"
+set :application, "ondoku-star"
 set :repo_url, "git@github.com:chain792/read_aloud_checker.git"
 
 # Default branch is :master
