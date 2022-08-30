359e519e0660caefea333827574131de9709a637
itoshusu <sopfantasy@yahoo.co.jp>
2022-08-31 01:49:50 +0900

    add: 履歴詳細ページのスタイル追加

--- front/src/components/pages/history/show.vue
+++ front/src/components/pages/history/show.vue
@@ -1,28 +1,45 @@
 <template>
-  <v-container>
-    <h1 class="text-center text-h5">{{ sentence.title }}</h1>
-    <v-card variant="outlined" :elevation="2" class="mx-auto mt-5 px-5 py-3">
-      <v-card-text class="mt-3">
-      <div class="sentence-body text-h6" v-html="sentence.body"></div>   
-      </v-card-text>
-    </v-card>
-    <div v-if="isSavedVoice" class="d-flex justify-center">
-      <audio ref="audio"  controls></audio>
-    </div>
-    <div class="mt-5 d-flex justify-center">
-      <v-btn :border="true" @click="linkToSentence">この英文を音読する</v-btn>
-      <v-btn :border="true">音声を再生する</v-btn>
-    </div>
-  </v-container>
+  <div class="page-history">
+    <v-container class="sentence-container">
+      <h1 class="text-center text-h5 sentence-title">{{ sentence.title }}</h1>
+      <v-card variant="outlined" :elevation="2" class="bg-white mx-auto mt-4 px-sm-5 flex-shrink-1 overflow-y-auto">
+        <v-card-text class="mt-3">
+        <div class="sentence-body text-h6" v-html="sentence.body"></div>   
+        </v-card-text>
+      </v-card>
+        <div class="d-sm-flex justify-center mt-3">
+          <div v-if="isSavedVoice" class="mr-0 mr-sm-1">
+            <p class="text-body-2 text-center mb-1">音読した音声</p>
+            <div class="text-center"><audio ref="audio" controls></audio></div>
+          </div>
+          <div class="ml-0 ml-sm-1 mt-2 mt-sm-0">
+            <p class="text-body-2 text-center mb-1">お手本の音声</p>
+            <div class="text-center"><audio :src="speechUrl(sentence, userStore.authUser)"  controls></audio></div>
+          </div>
+        </div>
+      <div class="mt-5 d-flex justify-center">
+        <v-btn 
+          color="warning"
+          width="300"
+          @click="linkToSentence"
+        >
+          この英文を音読する
+        </v-btn>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onBeforeUnmount } from "vue"
 import axios from "@/plugins/axios"
 import { useRouter } from 'vue-router'
-import { toWords } from "number-to-words";
+import { toWords } from "number-to-words"
+import { speechUrl } from "@/common/speechUrl"
+import { useUserStore } from "@/store/userStore"
 
 const router = useRouter()
+const userStore = useUserStore()
 
 interface Props {
   id: string
@@ -36,7 +53,9 @@ const props = defineProps<Props>()
 const sentence = ref({
   id: "",
   title: "",
-  body: ""
+  body: "",
+  maleSpeech: "",
+  femaleSpeech: "",
 })
 const isSavedVoice = ref(false)
 const audio = ref<HTMLAudioElement>()
@@ -100,8 +119,34 @@ onBeforeUnmount(() => {
 </script>
 
 <style scoped>
+.page-history{
+  background-color: rgba(225, 200, 30, 0.1);
+  height: 100%;
+}
+
+.sentence-container{
+  height: calc(100vh - 64px) !important;
+}
+
+.sentence-title{
+  font-size: 1.3rem !important;
+}
+
 .sentence-body{
   font-weight: 350;
   line-height: 1.5;
+  font-size: 1.1rem !important;
+}
+
+@media (min-width: 600px) {
+  .sentence-container{
+    height: calc(100vh - 64px - 92px) !important;
+  }
+  .sentence-title{
+    font-size: 1.5rem !important;
+  }
+  .sentence-body{
+    font-size: 1.25rem !important;
+  }
 }
 </style>
