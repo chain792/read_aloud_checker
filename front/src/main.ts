ebffff11ef071332f95baad68f4550dcf7197635
itoshusu <sopfantasy@yahoo.co.jp>
2022-09-03 18:44:26 +0900

    add: ga4を導入

--- front/src/main.ts
+++ front/src/main.ts
@@ -4,7 +4,18 @@ import router from "./router"
 import vuetify from "./plugins/vuetify"
 import { loadFonts } from "./plugins/webfontloader"
 import { createPinia } from "pinia"
+import VueGtag from "vue-gtag";
 
 loadFonts()
 
-createApp(App).use(router).use(vuetify).use(createPinia()).mount("#app")
+const app = createApp(App).use(router)
+
+if(import.meta.env.PROD){
+  app.use(VueGtag, {
+    config: { 
+      id: import.meta.env.VITE_GA4_ID,
+    },
+  }, router)
+}
+
+app.use(vuetify).use(createPinia()).mount("#app")
