b6242f52c4e1f8dfdc3b1aa727977543f8c4d692
itoshusu <sopfantasy@yahoo.co.jp>
2022-09-03 16:21:34 +0900

    add: デフォルトのアバターを変更

--- front/src/common/imageUrl.ts
+++ front/src/common/imageUrl.ts
@@ -9,7 +9,7 @@ export function imageUrl(type: ImageType, object: S3Object): string {
   switch (type) {
     case 'avatar':
       if(!object.avatar){
-        return '/cat.jpeg'
+        return '/default.png'
       }
       imagePath = `user/avatar/${object.id}/${object.avatar}`
   }
