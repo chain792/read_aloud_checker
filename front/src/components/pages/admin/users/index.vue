75e64f75c3c44de7fe4953abafd3117fc5b4914d
itoshusu <sopfantasy@yahoo.co.jp>
2022-09-24 18:13:47 +0900

    add: ユーザー一覧のページネーション機能を追加

--- front/src/components/pages/admin/users/index.vue
+++ front/src/components/pages/admin/users/index.vue
@@ -36,12 +36,20 @@
         </tr>
       </tbody>
     </v-table>
+    <v-pagination
+      v-if="paginationLength > 1"
+      v-model="page"
+      class="mt-4 mb-2"
+      :length="paginationLength"
+      @update:model-value="paginate(page as number)"
+    ></v-pagination>
   </v-container>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue"
 import axios from "@/plugins/axios"
+import { useRouter, useRoute, onBeforeRouteUpdate } from "vue-router"
 
 interface User {
   id: number
@@ -51,19 +59,35 @@ interface User {
   createdAt: string
 }
 
+const router = useRouter()
+const route = useRoute()
 
 const users = ref<Array<User>>([])
 
+const queryValueOfPage = route.query.page? Number(route.query.page) : undefined
+const page = ref(queryValueOfPage)
+const paginationLength = ref(1)
 
-const fetchUsers = async (): Promise<void> => {
+const fetchUsers = async (page?: string | number): Promise<void> => {
   try{
-    const res = await axios.get(`admin/users`)
+    const query =  page ? `?page=${page}` : ''
+    const res = await axios.get("admin/users" + query)
+    paginationLength.value= res.data.pagination.pages
     users.value = res.data.users
   } catch(e) {
     console.log(e)
   }
 }
-fetchUsers()
+fetchUsers(queryValueOfPage)
+
+const paginate = (page: number) => {
+  router.push({ name: "AdminUsers", query: { page } })
+}
+
+onBeforeRouteUpdate(async (to) => {
+  users.value = []
+  await fetchUsers(to.query.page as string)
+})
 
 </script>
 
