0de740611d94e9ced45b8fbb2659189cfd493dfd
itoshusu <sopfantasy@yahoo.co.jp>
2022-10-10 23:27:46 +0900

    rubocopを修正

--- api/app/controllers/api/v1/user/records_controller.rb
+++ api/app/controllers/api/v1/user/records_controller.rb
@@ -1,28 +1,27 @@
 class Api::V1::User::RecordsController < ApplicationController
   def index
-    succeeded_results = current_user.trainings.in_month(params[:year], params[:month])
-      .joins(:result_words).merge(ResultWord.succeeded)
-      .select('date(result_words.created_at) as date, count(*) as count')
-      .group('date').order('date asc').to_a
-    failed_results = current_user.trainings.in_month(params[:year], params[:month])
-      .joins(:result_words).merge(ResultWord.failed)
-      .select('date(result_words.created_at) as date, count(*) as count')
-      .group('date').order('date asc').to_a
+    succeeded_results = current_user.trainings.in_month(params[:year], params[:month]).succeeded_counts
+    failed_results = current_user.trainings.in_month(params[:year], params[:month]).failed_counts
     day = Date.new(params[:year].to_i, params[:month].to_i, -1).day
 
     monthly_results = []
     1.upto day do |i|
-      succeeded_number = 0
-      failed_number = 0
-      if !succeeded_results.empty? && succeeded_results[0].date.day == i
-        succeeded_number = succeeded_results.shift.count
-      end
-      if !failed_results.empty? && failed_results[0].date.day == i
-        failed_number = failed_results.shift.count
-      end
+      succeeded_number = calculate_word_count(succeeded_results, i)
+      failed_number = calculate_word_count(failed_results, i)
+
       monthly_results << { succeededNumber: succeeded_number, failedNumber: failed_number }
     end
 
     render json: monthly_results
+  end
+
+  private
+
+  def calculate_word_count(results, day)
+    if !results.empty? && results[0].date.day == day
+      results.shift.count
+    else
+      0
+    end
   end
 end
