9e40937615322b49f77c7bd70019f4c6eb611db5
itoshusu <sopfantasy@yahoo.co.jp>
2022-10-10 10:41:45 +0900

    add: rubocopファイルを編集

--- api/app/controllers/api/v1/profile/passwords_controller.rb
+++ api/app/controllers/api/v1/profile/passwords_controller.rb
@@ -1,10 +1,7 @@
 class Api::V1::Profile::PasswordsController < ApplicationController
-  def update  user = User.find(current_user.id)
+  def update  
+    user = User.find(current_user.id)
 
-  
-    
-
-    
     if user.valid_password?(params[:current_password])
       if user.update(password: params[:password], password_confirmation: params[:password_confirmation])
         render json: UserResource.new(user).serialize
