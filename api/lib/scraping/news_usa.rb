24702de5f7e00902370cb43fc5c7e974c7979468
itoshusu <sopfantasy@yahoo.co.jp>
2022-10-24 05:45:01 +0900

    add: サムネイルも取得するようスクレイピング処理を変更

--- api/lib/scraping/news_usa.rb
+++ api/lib/scraping/news_usa.rb
@@ -1,18 +1,25 @@
 module Scraping
   class NewsUSA
-    def self.scrape
-      url = 'https://www.copyrightfreecontent.com/'
-      html = URI.parse(url).open.read
+    def self.scrape(n)
+      base_url = 'https://www.copyrightfreecontent.com/'
+      html = URI.parse(base_url + "page/#{n}/").open.read
       doc = Nokogiri::HTML.parse(html)
       news = []
       doc.css('#content > div').each do |div|
         break if div.attribute('class').value == 'navigation'
+        
+        body = div.at_css('.entry > p').text.gsub(/\(NewsUSA\)\W*/, '')
+        # 長すぎる英文は取得しない 
+        next if body.split.length > 300
 
         title = div.at_css('h1 > a').text
-        body = div.at_css('.entry > p').text.gsub(/\(NewsUSA\)\W*/, '')
-        category = div.at_css('h1 > a').attribute('href').value.gsub(url, '').gsub(%r{/.*}, '')
-        news << { title: title, body: body, category: category }
+        thumbnail = div.at_css('.entry > p > .category-img').attribute('src').value
+        category = div.at_css('h1 > a').attribute('href').value.gsub(base_url, '').gsub(%r{/.*}, '')
+        news << { title: title, body: body, category: category, thumbnail: thumbnail }
       end
+      
+      binding.pry
+      
       news
     end
   end
