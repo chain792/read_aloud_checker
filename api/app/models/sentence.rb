c265e5568e9abaa5be12f28acbf089751241e3a1
itoshusu <sopfantasy@yahoo.co.jp>
2022-10-24 04:11:09 +0900

    add: word_countの計算処理を追加

--- api/app/models/sentence.rb
+++ api/app/models/sentence.rb
@@ -14,6 +14,7 @@ class Sentence < ApplicationRecord
 
   # 全角を半角にする
   before_save -> { self.body = NKF.nkf('-w -Z1 -Z4 -x', body) }
+  before_save -> { self.word_count = body.split.length }
   after_create :create_speech
   after_update :create_speech, if: :saved_change_to_body?
 
